c2c9ca6150d2b358b7167e72f357f1c639ed72ef
steamjin <kkjj2024@naver.com>
2025-09-08 15:00:17 +0900

    chap03-method-and-api 1번

---
+++ src/main/java/com/steamjin/level02/normal/RandomMaker.java
@@ -0,0 +1,10 @@
+package com.steamjin.level02.normal;
+
+public class RandomMaker {
+    public static void main(String[] args) {
+
+    } public int randomNumber(int min, int max) {
+    int random = 0;
+        return random;
+    }
+}
